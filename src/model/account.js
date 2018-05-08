3209c3f33d12119b87d997c7b251635eaae85cb2
Joshua Fredrickson <uafredrickson@gmail.com>
2018-05-07 19:02:31 -0700

    demo code added

--- src/model/account.js
+++ src/model/account.js
@@ -6,7 +6,7 @@ import crypto from 'crypto';
 import jsonWebToken from 'jsonwebtoken';
 
 const HASH_ROUNDS = 8;
-const TOKEN_SEE_LENGTH = 128;
+const TOKEN_SEED_LENGTH = 128;
 
 const accountSchema = mongoose.Schema({
   passwordHash: {
@@ -30,7 +30,7 @@ const accountSchema = mongoose.Schema({
 });
 
 function pCreateToken() {
-  this.tokenSeed = crypto.randomBytes(TOKEN_SEE_LENGTH).toString('hex');
+  this.tokenSeed = crypto.randomBytes(TOKEN_SEED_LENGTH).toString('hex');
   return this.save()
     .then((account) => {
       return jsonWebToken.sign({ tokenSeed: account.tokenSeed }, process.env.STUFF_SECRET);
@@ -38,3 +38,21 @@ function pCreateToken() {
 }
 
 accountSchema.methods.pCreateToken = pCreateToken;
+
+const Account = mongoose.model('account', accountSchema);
+
+Account.create = (username, email, password) => {
+  return bcrypt.hash(password, HASH_ROUNDS)
+    .then((passwordHash) => {
+      password = null;  // eslint-disable-line
+      const tokenSeed = crypto.randomBytes(TOKEN_SEED_LENGTH).toString('hex');
+      return new Account({
+        username,
+        email,
+        passwordHash,
+        tokenSeed,
+      }).save();
+    });
+};
+
+export default Account;
