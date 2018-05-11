e5e7b4690976ee831999300779ceaa2f88dbdd14
danielvportlandia <danielshelton90@gmail.com>
2018-05-11 08:55:48 -0700

    200 test working for image

--- src/lib/server.js
+++ src/lib/server.js
@@ -2,11 +2,12 @@
 
 import express from 'express';
 import mongoose from 'mongoose';
-import logger from './logger';
 import loggerMiddleware from './logger-middlware';
-import errorMiddleware from './error-middleware';
+import logger from './logger';
 import authRoutes from '../route/auth-router';
 import profileRoutes from '../route/profile-route';
+import imageRoutes from '../route/image-router';
+import errorMiddleware from './error-middleware';
 
 const app = express();
 let server = null;
@@ -14,6 +15,7 @@ let server = null;
 app.use(loggerMiddleware);
 app.use(authRoutes);
 app.use(profileRoutes);
+app.use(imageRoutes);
 
 app.all('*', (request, response) => {
   logger.log(logger.INFO, 'Returning a 404 from the catch-all/default route');
